d1d9f2bc35713f2ae05d9acff0b6526d03628f60
Darylxyx <921218060@qq.com>
2017-07-25 12:08:12 +0800

    update

--- fragment/index.js
+++ fragment/index.js
@@ -1,3 +1,14 @@
 window.onload = function() {
-	
-}
+	var ctx = document.querySelector('#myCanvas').getContext('2d'),
+		img = document.querySelector('#imgA');
+
+	ctx.beginPath();
+
+    ctx.drawImage(img, 0, 0, 960, 540, 0, 0, 400, 225);
+    ctx.drawImage(img, 960, 0, 960, 540, 400, 0, 400, 225);
+    ctx.drawImage(img, 0, 540, 960, 540, 0, 225, 400, 225);
+    ctx.drawImage(img, 960, 540, 960, 540, 400, 225, 400, 225);
+    
+    ctx.closePath();
+    ctx.stroke();
+};
